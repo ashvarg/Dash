ed4fbd2e571323a05bc59edc34a4b6161e6eeee1
Ashwin Varughese George <ageo0015@student.monash.edu>
2022-09-02 13:05:55 +1000

    added a main.js file

---
+++ Projects/scripts/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// array of cards
+
+array = [];
+
+// just for opening the card into card view 
+function openCard(){
+    document.getElementById("myCard").style.display = "block";
+}
+
+function closeCard(){
+    document.getElementById("myCard").style.display = "none";
+    
+}
