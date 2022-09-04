d2285e153fbe37a8f7f85bf365e787e3099e631b
ahee0011 <ahee0011@student.monash.edu>
2022-09-04 02:58:22 +0000

    Create & Display Task Cards

--- Projects/scripts/taskClass.js
+++ Projects/scripts/taskClass.js
@@ -1,5 +1,7 @@
 "use strict";
-export class task {
+
+//Don't need to import into other files, as long as we have link in HTML files they are accessible
+class task {
     constructor(name, type, storyPoints, tag, priority, assignee, description, status) {
         this._name = name;
         this._type = type;
