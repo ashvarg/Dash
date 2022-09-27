5e164a695e382342955fa713d3e5da5be0c5cc0d
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2022-09-27 22:37:26 +1000

    Create Intuitive Way To Switch Toggle Text

--- Projects/scripts/sprint.js
+++ Projects/scripts/sprint.js
@@ -1,2 +1,16 @@
 "use strict";
 
+function toggleViewLabel(){
+    
+    //Get the inner html
+    let toggleText = document.getElementById("toggleLabel");
+    
+    //Switch the text value depending on what is there
+    if (toggleText.textContent == "Kanban"){
+        toggleText.innerHTML = "Chart";
+    }
+    else if(toggleText.textContent == "Chart"){
+        toggleText.innerHTML = "Kanban";
+    }
+
+}
