28cc4bccd94d2750438926341a77a56aac983ef6
Arosh Heenkenda <arosh.heenkenda@outlook.com>
2022-09-27 23:36:52 +1000

    Design Form To Create Sprint, No Functionality Yet

--- Projects/scripts/sprint.js
+++ Projects/scripts/sprint.js
@@ -4,13 +4,28 @@ function toggleViewLabel(){
     
     //Get the inner html
     let toggleText = document.getElementById("toggleLabel");
-    
+
     //Switch the text value depending on what is there
     if (toggleText.textContent == "Kanban"){
+        //Switch to Chart
         toggleText.innerHTML = "Chart";
     }
     else if(toggleText.textContent == "Chart"){
+        //Switch to Kanban
         toggleText.innerHTML = "Kanban";
     }
 
+}
+
+
+function createSprint(){
+
+        let modal_container = document.getElementById("createSprintForm");
+        modal_container.classList.add("show");
+}
+
+function createSprintClose(){
+
+    let modal_container = document.getElementById("createSprintForm");
+    modal_container.classList.remove("show");
 }
